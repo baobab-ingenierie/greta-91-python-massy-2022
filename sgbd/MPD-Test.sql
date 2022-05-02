157c062ab23195f4464f838ba7df635d244cb885
lesly-lodin <lesly.lodin@baobab-ingenierie.fr>
2022-05-02 15:05:50 +0200

    DBMS Day 3

---
+++ sgbd/MPD-Test.sql
@@ -0,0 +1,13 @@
+-- Lister les BDD
+SHOW databases;
+
+-- Utilise (entre dans) la BDD
+USE mysql;
+SHOW tables;
+
+-- Supprime la BDD si elle existe 
+-- (en phase de test SEULEMENT)
+DROP DATABASE IF EXISTS massy;
+
+-- Crée une BDD utilisateur
+CREATE DATABASE massy;
